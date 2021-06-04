aa51e37884257b081c6adac947a2f3bc7ebe9e48
Jason Ou <catopia.app@gmail.com>
2021-06-04 14:56:29 +0800

    Add admin target

---
+++ EcommerceAdmin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EcommerceAdmin
+//
+//  Created by Jason Ou on 2021/6/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
